91cb5e0586a74b01d1b2f32056d9752ea95d1a47
arorasukhmani <91721651+arorasukhmani@users.noreply.github.com>
2024-08-03 11:50:35 +0530

    headerFooter

--- script.js
+++ script.js
@@ -3,6 +3,45 @@ const playIcon = document.querySelector(".play-icon");
 const pauseIcon = document.querySelector(".pause-icon");
 const toggleButton = document.querySelector(".toggle-button");
 
+
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+};
+
+
+let sections = document.querySelectorAll('section');
+let NavLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            NavLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+// Sticky NavBar
+
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+menuIcon.classList.remove('bx-x');
+navBar.classList.remove('active');
+
+};
+
 function toggleRotation() {
   const animationState = getComputedStyle(cube).animationPlayState;
 
